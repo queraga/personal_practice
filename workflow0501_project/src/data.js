7435c96cc37d19c152b4b9ec2e31a0a3d2488783
Lashkov Yevhen <evgene@icloud.com>
2026-01-05 19:30:39 +0100

    Project Sneaker Shop Setup

---
+++ workflow0501_project/src/data.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "360",
+    id: 1,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "234",
+    id: 2,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "134",
+    id: 3,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "175",
+    id: 4,
+  },
+];
+
+// fetchCartItems GET "/cartData"
+// addCart POST "/cartData"
+// deleteFromCart DELETE "/cartData/${id}"
